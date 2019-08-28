33155701bb4b67294522c9f868cd61279086d141
Jayashree <jayashree.a@cgi.com>
2019-08-28 12:01:47 +0530

    adapter code updated

---
+++ src/main/java/com/stackroute/succour/fileReaderAdapter/domain/NewsAPIResponseObject.java
@@ -0,0 +1,16 @@
+//package com.stackroute.succour.fileReaderAdapter.domain;
+//
+//import lombok.*;
+//
+///**
+// * Entity object to store the response from newapi.org
+// */
+//@Data
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//public class NewsAPIResponseObject {
+//    private String entities;
+//    private TwitterTimeline[] twitterTimelines;
+//}
